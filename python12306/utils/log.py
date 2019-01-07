716c441b48a7697a21506ac9a33848febd41c3f8
fredzhang <versionzhang@gmail.com>
2019-01-07 14:19:41 +0800

    change log file

--- python12306/utils/log.py
+++ python12306/utils/log.py
@@ -1,31 +1,58 @@
+import os
+import logging.handlers
+
 from colorama import Fore
 
+
+logger = logging.getLogger()
+ticket_logger = logging.getLogger('ticket')
+ticket_logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '[%(asctime)s] p%(process)s %(pathname)s:%(lineno)d %(levelname)s - %(message)s', '%Y-%m-%d %H:%M:%S'
+)
+
+# Add the log message handler to the logger
+handler = logging.handlers.RotatingFileHandler(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), '../logs/buy_ticket.log'),
+                    maxBytes=20*1024*1024, backupCount=5)
+handler.setFormatter(formatter)
+ticket_logger.addHandler(handler)
 
 class LogUtils(object):
 
     @staticmethod
-    def __print(msg, color):
+    def __print(msg, msg_type):
+        """
+        :param msg: log text.
+        :param color: [Fore.GREEN, Fore.BLUE, Fore.YELLOW, Fore.RED]
+        :param msg_type: [info, debug, warning, error]
+        :return:
+        """
+        maping = dict(info=Fore.GREEN, debug=Fore.BLUE, warning=Fore.YELLOW, error=Fore.RED)
+        l = getattr(ticket_logger, msg_type)
         if type(msg) == str:
-            print(color + msg + Fore.RESET)
+            print(maping[msg_type] + msg + Fore.RESET)
+            l(msg)
         else:
-            print(color)
+            print(maping[msg_type])
             print(msg)
             print(Fore.RESET)
+            l(msg)
 
     def d(self, msg):
         # debug msg
-        self.__print(msg, Fore.BLUE)
+        self.__print(msg, 'debug')
 
     def v(self, msg):
         # verbose msg
-        self.__print(msg, Fore.GREEN)
+        self.__print(msg, 'info')
 
     def w(self, msg):
         # warning msg
-        self.__print(msg, Fore.YELLOW)
+        self.__print(msg, 'warning')
 
     def e(self, msg):
         # error msg
-        self.__print(msg, Fore.RED)
+        self.__print(msg, 'error')
 
 Log = LogUtils()
