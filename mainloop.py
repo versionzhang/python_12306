4ddf43c43af879fac4b4dab4d2944bd2c7c01a1b
songroger <songroger@vip.qq.com>
2019-01-16 11:05:37 +0800

    mutil_thread_test

--- mainloop.py
+++ mainloop.py
@@ -1,10 +1,14 @@
+import sys
+
 from python12306.scheduler import Schedule
+from python12306.threadscheduler import Schedule as Multischedule
 
 
-def main():
-    instance = Schedule()
+def main(mode):
+    instance = Multischedule() if mode == "multi" else Schedule()
     instance.run()
 
 
 if __name__ == "__main__":
-    main()
+    cmd = sys.argv[1].lower() if sys.argv[1:] else ''
+    main(cmd)
