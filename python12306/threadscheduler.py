05d0dba64deaa59d7fdb2c69552ca53843ded5a2
fredzhang <versionzhang@gmail.com>
2019-01-17 10:00:52 +0800

    修改使用标准库的线程池

--- python12306/threadscheduler.py
+++ python12306/threadscheduler.py
@@ -1,65 +1,12 @@
 import datetime
 import time
 from queue import Queue
-from threading import Thread
+from multiprocessing.pool import ThreadPool
 
 from python12306.config import Config
 
-from python12306.logic.login.checkuser import OnlineCheckerTool
-from python12306.logic.login.login import NormalLogin
-from python12306.logic.login.passager import QueryPassengerTool
 from python12306.logic.query.dispatcher import DispatcherTool
-from python12306.logic.submit.fastsubmit import FastSubmitDcOrder
-from python12306.logic.submit.submit import NormalSubmitDcOrder
-
 from python12306.scheduler import Schedule as BaseSchedule
-
-from python12306.utils.send_email import send_email
-from python12306.utils.log import Log
-
-
-class Worker(Thread):
-    """ Thread executing tasks from a given tasks queue """
-
-    def __init__(self, tasks):
-        Thread.__init__(self)
-        self.tasks = tasks
-        self.daemon = True
-        self.start()
-
-    def run(self):
-        while True:
-            func, args, kargs = self.tasks.get()
-            try:
-                func(*args, **kargs)
-            except Exception as e:
-                # An exception happened in this thread
-                print(e)
-            finally:
-                # Mark this task as done, whether an exception happened or not
-                self.tasks.task_done()
-
-
-class ThreadPool(object):
-    """ Pool of threads consuming tasks from a queue """
-
-    def __init__(self, num_threads):
-        self.tasks = Queue(num_threads)
-        for _ in range(num_threads):
-            Worker(self.tasks)
-
-    def add_task(self, func, *args, **kargs):
-        """ Add a task to the queue """
-        self.tasks.put((func, args, kargs))
-
-    def map(self, func, args_list):
-        """ Add a list of tasks to the queue """
-        for args in args_list:
-            self.add_task(func, args)
-
-    def wait_completion(self):
-        """ Wait for completion of all the tasks in the queue """
-        self.tasks.join()
 
 
 class Schedule(BaseSchedule):
@@ -84,7 +31,7 @@ class Schedule(BaseSchedule):
 
             dates = DispatcherTool.query_travel_dates
             self.pool.map(self.batch_tasks, dates)
-            self.pool.wait_completion()
+            self.pool.close()
             if self.order_id or self.unfinished_order:
                 break
         self.notice_user()
