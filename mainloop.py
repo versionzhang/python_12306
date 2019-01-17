8a53985ab9bba5eaa345cdcba1c9b9f4e157ddfb
fredzhang <versionzhang@gmail.com>
2019-01-17 10:10:31 +0800

    修改调用方法

--- mainloop.py
+++ mainloop.py
@@ -1,14 +1,12 @@
-import sys
-
 from python12306.scheduler import Schedule
 from python12306.threadscheduler import Schedule as Multischedule
+from python12306.config import Config
 
 
-def main(mode):
-    instance = Multischedule() if mode == "multi" else Schedule()
+def main():
+    instance = Multischedule() if Config.multi_threading_enable else Schedule()
     instance.run()
 
 
 if __name__ == "__main__":
-    cmd = sys.argv[1].lower() if sys.argv[1:] else ''
-    main(cmd)
+    main()
